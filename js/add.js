833a3e61b77174b79da6ea095dce8b0dae307bbe
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-09-02 08:54:53 -0700

    add examples of using push to add items to the end of an array, and using unshift to add items to the beginning of an array.

--- js/add.js
+++ js/add.js
@@ -1,1 +1,3 @@
 const instruments = ['piano', 'drums', 'trumpet'];
+instruments.push('guitar', 'violin', 'triangle'); //adds to the end
+instruments.unshift('cowbell', 'tuba'); //adds to the beginning
